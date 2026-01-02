951ddd75bc1bb9174418e8020d905381577faf3a
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-02 17:36:11 -0600

    made the components for auth

--- frontend/luv-note/app/(auth)/login/page.tsx
+++ frontend/luv-note/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
- import React from 'react'
- 
- const page = () => {
-   return (
-     <div>page</div>
-   )
- }
- 
- export default page
+import React from 'react'
+
+const page = () => {
+return (
+    <div>page</div>
+)
+}
+
+export default page
